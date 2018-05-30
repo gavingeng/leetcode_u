3f932d5d7e50f9c3be1fbac6666cab6dfd8a7fb0
gavingeng <gavingeng@hotmail.com>
2018-05-30 23:16:57 +0800

    1.node非空处理;2.空的next==null

--- src/add_two_numbers/AddTwoNumbers.java
+++ src/add_two_numbers/AddTwoNumbers.java
@@ -11,12 +11,14 @@ public class AddTwoNumbers {
 		//进位
 		int carry = 0;
 		while (l1 != null || l2 != null) {
-			int v = carry + l1.val + l2.val;
+			int x = l1 == null ? 0 : l1.val;
+			int y = l2 == null ? 0 : l2.val;
+			int v = carry + x + y;
 			curNode.next = new ListNode(v % 10);
 			carry = v / 10;
 			curNode = curNode.next;
-			l1 = l1.next;
-			l2 = l2.next;
+			l1 = l1 == null ? null : l1.next;
+			l2 = l2 == null ? null : l2.next;
 		}
 		return retNode.next;
 	}
@@ -57,6 +59,7 @@ public class AddTwoNumbers {
 		ListNode l2 = new ListNode(5);
 		l2.next = new ListNode(6);
 		l2.next.next = new ListNode(4);
+		l2.next.next.next = new ListNode(9);
 
 		ListNode ret = addTwoNumbers(l1, l2);
 		ListNode head = reverse(ret);
