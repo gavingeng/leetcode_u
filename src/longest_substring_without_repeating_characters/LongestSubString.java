93b2dbded872293838d0806ee37ac231231859b4
gavingeng <gavingeng@hotmail.com>
2018-06-01 01:05:07 +0800

    最长无重复字符串-->最长无重复字符串的长度

---
+++ src/longest_substring_without_repeating_characters/LongestSubString.java
@@ -0,0 +1,34 @@
+package longest_substring_without_repeating_characters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * link: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ * Created by gavingeng on 2018-05-31 16:07.
+ */
+public class LongestSubString {
+
+	public static int lengthOfLongestSubstring(String s) {
+		int length = s.length();
+		int ret = 0;
+		//key:字符 value:索引
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0, j = 0; j < length; j++) {
+			if (map.containsKey(s.charAt(j))) {
+				i = map.get(s.charAt(j));
+			}
+			ret = Math.max(ret, j - i + 1);
+			map.put(s.charAt(j), j + 1);
+		}
+
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		String s = "abcabcbb";
+		s = "bbbbb";
+		int size = lengthOfLongestSubstring(s);
+		System.out.println("size=" + size);
+	}
+}
---
+++ src/longest_substring_without_repeating_characters/README.md
@@ -0,0 +1,10 @@
+##longest_substring_without_repeating_characters
+```
+最长无重复字符串的大小
+link:
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+刚开始审题审偏差了，审成 "最长无重复字符串"，一直在这个思路上绕来绕去的，耽误了很长时间
+
+
+```
