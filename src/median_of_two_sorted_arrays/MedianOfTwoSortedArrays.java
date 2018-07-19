3d123f729834d2e55672a475f879258f636b0002
gavingeng <gavingeng@hotmail.com>
2018-07-19 11:02:04 +0800

    合并有序数组

--- src/median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
+++ src/median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
@@ -5,13 +5,59 @@ package median_of_two_sorted_arrays;
  * Created by gavingeng on 2018-06-01 11:54.
  */
 public class MedianOfTwoSortedArrays {
-
-
+	
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
+		int m = nums1.length;
+		int n = nums2.length;
+		//确保m<=n
+		if (m > n) {
+			int[] temp = nums1;
+			nums1 = nums2;
+			nums2 = temp;
+			
+			int tmp = m;
+			m = n;
+			n = tmp;
+		}
+		
+		int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+		while (iMin <= iMax) {
+			int i = (iMin + iMax) / 2;
+			int j = halfLen - i;
+			
+			if (i < iMax && nums2[j - 1] > nums1[i]) {
+				iMin = iMin + 1;
+			} else if (i > iMin && nums1[i - 1] > nums2[j]) {
+				iMax = iMax - 1;
+			} else {
+				int maxLeft = 0;
+				if (i == 0) {
+					maxLeft = nums2[j - 1];
+				} else if (j == 0) {
+					maxLeft = nums1[i - 1];
+				} else {
+					maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+				}
+				if ((m + n) % 2 == 1) {
+					return maxLeft;
+				}
+				
+				int minRight = 0;
+				if (i == m) {
+					minRight = nums2[j];
+				} else if (j == n) {
+					minRight = nums1[i];
+				} else {
+					minRight = Math.max(nums2[j], nums1[i]);
+				}
+				
+				return (maxLeft + minRight) / 2.0;
+			}
+		}
+		
 		return 0.0;
 	}
-
+	
 	public static void main(String[] args) {
 		int[] nums1 = {1, 2};
 		int[] nums2 = {3, 4, 5};
--- src/median_of_two_sorted_arrays/README.md
+++ src/median_of_two_sorted_arrays/README.md
@@ -6,4 +6,11 @@ link: https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
 逻辑判断没成功，还得再处理，再找其他时间处理！
 
+写的太复杂，也容易出错，参考了 https://leetcode.com/problems/median-of-two-sorted-arrays/solution/
+
+其他的一些借鉴
+https://blog.csdn.net/hk2291976/article/details/51107778
+https://blog.csdn.net/hk2291976/article/details/51107543
+看这里的讲解才有了明白
+
 ```
